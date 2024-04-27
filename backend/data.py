b61babe477a451285292778ea820463f9bebb384
Evgenij Ivankin <e.ivankin@innopolis.university>
2024-04-27 18:20:33 +0300

    add data types

--- backend/data.py
+++ backend/data.py
@@ -1,10 +1,18 @@
-from enum import IntEnum
-from typing import Annotated
+from datetime import datetime
+from enum import IntEnum, StrEnum
+from typing import Annotated, NewType
 
-from annotated_types import Ge
+from annotated_types import Ge, Le
 from pydantic import BaseModel, Field
 
 UTMCoordinate = Annotated[float, Ge(0)]
+GlobalCoordinate = Annotated[float, Ge(-180), Le(180)]
+VehicleID = NewType("VehicleID", int)
+
+
+class TrackedVehicleStatus(StrEnum):
+    ACTIVE = "active"
+    PASSED = "passed"
 
 
 class VehicleType(IntEnum):
@@ -17,6 +25,19 @@ class VehicleType(IntEnum):
 
 
 class TrafficMessage(BaseModel):
-    unix_millis: ...
+    unix_millis: datetime
     center: tuple[UTMCoordinate, UTMCoordinate]
-    class_: ... = Field(alias="class")
+    class_: VehicleType = Field(alias="class")
+
+
+class TrackedPosition(BaseModel):
+    timestamp: datetime
+    lat: GlobalCoordinate
+    lon: GlobalCoordinate
+
+
+class TrackedVehicle(BaseModel):
+    vehicle_id: VehicleID
+    vehicle_class: VehicleType
+    vehicle_path: list[TrackedPosition]
+    status: TrackedVehicleStatus
